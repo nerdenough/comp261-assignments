3aeb8cea686ec0b3645c11d15eb72e537ca2d29c
Brendan Goodenough <brendan@goodenough.nz>
2016-03-18 20:10:16 +1300

    Fix pan direction for the graph

--- assignment-1/src/comp261/assignment1/state/GraphState.java
+++ assignment-1/src/comp261/assignment1/state/GraphState.java
@@ -18,10 +18,10 @@ public class GraphState extends State {
 	 * @see comp261.assignment1.state.State#update()
 	 */
 	public void update() {
-		if (keys[0]) graph.setGraphY(graph.getGraphY() + 4);
-		if (keys[1]) graph.setGraphY(graph.getGraphY() - 4);
-		if (keys[2]) graph.setGraphX(graph.getGraphX() + 4);
-		if (keys[3]) graph.setGraphX(graph.getGraphX() - 4);
+		if (keys[0]) graph.setGraphY(graph.getGraphY() - 4);
+		if (keys[1]) graph.setGraphY(graph.getGraphY() + 4);
+		if (keys[2]) graph.setGraphX(graph.getGraphX() - 4);
+		if (keys[3]) graph.setGraphX(graph.getGraphX() + 4);
 		if (keys[4]) Graph.zoom += 10;
 		if (keys[5]) Graph.zoom -= 10;
 		
